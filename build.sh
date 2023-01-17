381d0178952a063df2685765458cc4ec6fc7c2bf
kagaston <kody.gaston@gmail.com>
2023-01-16 20:53:37 -0700

    Updated build script and bootstrap script to removed unneeded lines and comments. Removed Pyspark due to its size and the implementation of scala for the jupyter notebook.

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
+APPNAME="jupyter"
+REPO="kagaston"
+SPARK_VERSION="3.3.1"
 
-docker build -t jupyter:3.3.1 .
+docker build -t "${REPO}/${APPNAME}:${SPARK_VERSION}" -t "${REPO}/${APPNAME}:latest" .
+docker push --all-tags ${REPO}/${APPNAME}
--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,40 +1,25 @@
 #!/usr/bin/env bash
+
+# TODO Remove the CS section to the build file
+#
 main () {
-  install_tini_init
-  install_java
+  install_linux_dependencies
   install_python
   create_app_user
   configure_app_dir
   install_python_dependencies
-  install_jupyter
   install_cs
   install_scala
   install_scala_jupyter_kernel
-  # generate_self_signed_key
 }
 
-install_tini_init () {
-  URI="https://github.com/krallin/tini/releases/download/${TINI_VERSION}/tini"
-  curl -sSfL $URI -o /tini
-  curl -sSfL ${URI}.asc -o /tini.asc
-  gpg --batch --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 595E85A6B1B4779EA4DAAEC70B588DFF0527A9B7
-  gpg --batch --verify /tini.asc /tini
-  chmod +x /tini
-}
-
-install_java () {
-  microdnf -y install java-11-openjdk ncurses
-  microdnf -y clean all
-}
-
-install_python () {
-  microdnf -y install python3 python3-pip
+install_linux_dependencies () {
+  microdnf -y install java-17-openjdk ncurses openssl procps python3 python3-pip
   microdnf -y clean all
   alias pip="python3 -m pip --no-cache-dir --upgrade --compile --user ${GUID}"
 }
 
 install_cs () {
-  # TODO add pgp and hash validation
   echo "Installing the Scala installer Coursier"
   mkdir -p /opt/coursier/
   curl -sSfL https://github.com/coursier/launchers/raw/master/cs-x86_64-pc-linux.gz | gzip -d > /opt/coursier/cs
@@ -43,28 +28,13 @@ install_cs () {
   export PATH="$PATH:/root/.local/share/coursier/bin"
 }
 
-#validate () {
-#  # TODO add validation of package pgp and hash
-#  curl -sSfL ${APACHE_URL}/spark/KEYS -o KEYS
-#  curl -sSfL ${URI}.asc -o apache-spark.tgz.asc
-#  gpg --import KEYS
-#  gpg --verify apache-spark.tgz.asc apache-spark.tgz
-#  # TODO write hash check
-#  curl -sSfL ${URI}.sha512 -o apache-spark.tgz.sha512
-#}
-
-
 install_scala () {
-  dnf install java-11-openjdk -y # scala scalac dependency
   cs install scala:${SCALA_VERSION} scalac:${SCALA_VERSION}
 }
 
 install_python_dependencies () {
-  pip install pip setuptools py4j
-}
-
-install_jupyter () {
-  pip install pyspark jupyterlab
+  pip install pip setuptools
+  pip install py4j jupyterlab # pyspark ##<-- Removed pyspark as it is not performant in cluster mode for spark
   export PATH=$PATH:/home/jupyter/.local/bin
 }
 
@@ -77,6 +47,7 @@ create_app_user () {
   groupadd --gid 1000 $GUID
   useradd  --uid 1000 \
            --gid $GUID \
+           --system \
            --shell /bin/bash \
            --create-home $GUID
 }
@@ -84,19 +55,16 @@ create_app_user () {
 configure_app_dir () {
   mkdir -p $JUPYTER_HOME
   chown $GUID:$GUID $JUPYTER_HOME
-  # chmod 770 $JUPYTER_HOME
-
+  chmod 770 $JUPYTER_HOME
 }
 
-# enable_logging_to_docker () {
-#  # TODO configure logging
-#  #  ln -sf /dev/stdout $SPARK_MASTER_LOG
-#  #  ln -sf /dev/stdout $SPARK_WORKER_LOG
-#}
-
 generate_self_signed_key () {
-  openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout mykey.key -out mycert.pem
-#  openssl dhparam -out /etc/ssl/certs/dhparam.pem 4096
+  echo "Creating self-signed cert and dhparam"
+  CERT_PATH="/etc/ssl/certs"
+  openssl req -x509 -nodes -days 365 -newkey rsa:4096 \
+    -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=www.example.com" \
+    -keyout ${CERT_PATH}/mykey.key -out ${CERT_PATH}/mycert.pem
+  openssl dhparam -out ${CERT_PATH}/dhparam.pem 4096
 }
 
 ###############
