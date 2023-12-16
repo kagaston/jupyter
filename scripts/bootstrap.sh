c0113f2586f2e0c7b4da9383e07cdf45d434c80d
Kody Gaston <43728989+kagaston@users.noreply.github.com>
2023-12-16 01:33:03 -0700

    Update bootstrap.sh: remove pyspark install, due to image size

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,54 +1,41 @@
 #!/usr/bin/env bash
 
+# Check if running as root
+if [[ $EUID == 0 ]]; then
+  # Install necessary packages
+  microdnf -y install java-11-openjdk ncurses openssl procps python3 python3-pip curl gzip
+  microdnf -y clean all
 
-#!/usr/bin/env bash
+  # Create a group and user
+  groupadd --gid 1000 $GUID
+  useradd  --uid 1000 \
+           --gid $GUID \
+           --shell /bin/bash \
+           --create-home $GUID
 
-if [[ $EUID == 0 ]]
-  then
-    microdnf -y install java-11-openjdk ncurses openssl procps python3 python3-pip curl gzip
-    microdnf -y clean all
+  # Create Jupyter directory and set permissions
+  mkdir -p $JUPYTER_HOME
+  chown $GUID:$GUID $JUPYTER_HOME
+  chmod 770 $JUPYTER_HOME
 
-    groupadd --gid 1000 $GUID
-    useradd  --uid 1000 \
-             --gid $GUID \
-             --shell /bin/bash \
-             --create-home $GUID
+  # Download and set up Coursier
+  curl -sSfL https://github.com/coursier/launchers/raw/master/cs-x86_64-pc-linux.gz | gzip -d > ${JUPYTER_HOME}/cs
+  chmod +x ${JUPYTER_HOME}/cs
 
-    mkdir -p $JUPYTER_HOME
-    chown $GUID:$GUID $JUPYTER_HOME
-    chmod 770 $JUPYTER_HOME
+  # Install Python packages
+  python3 -m pip --no-cache-dir install -U pip setuptools
+  python3 -m spylon_kernel install
+else
+  # Install Scala with Coursier
+  ${JUPYTER_HOME}/cs install cs scala:${SCALA_VERSION} scalac:${SCALA_VERSION}
 
-    curl -sSfL https://github.com/coursier/launchers/raw/master/cs-x86_64-pc-linux.gz | gzip -d > ${JUPYTER_HOME}/cs
-    chmod +x ${JUPYTER_HOME}/cs
-
-    python3 -m pip --no-cache-dir install -U pip setuptools
-    python3 -m pip --no-cache-dir install -U py4j pyspark jupyterlab spylon-kernel
-    python3 -m spylon_kernel install
-  else
-    ${JUPYTER_HOME}/cs install cs scala:${SCALA_VERSION} scalac:${SCALA_VERSION}
-    rm -rf ${JUPYTER_HOME}/cs ${JUPYTER_HOME}/bootstrap.sh
+  # Clean up downloaded files
+  rm -rf ${JUPYTER_HOME}/cs ${JUPYTER_HOME}/bootstrap.sh
 fi
 
-
-
-
-
-
-
-
-
-
-
-### "Creating self-signed cert and dhparam"
+# Uncomment and customize the following section if you need to create a self-signed certificate and dhparam
 #CERT_PATH="/etc/ssl/certs"
 #openssl req -x509 -nodes -days 365 -newkey rsa:4096 \
 #  -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=www.example.com" \
 #  -keyout ${CERT_PATH}/mykey.key -out ${CERT_PATH}/mycert.pem
 #openssl dhparam -out ${CERT_PATH}/dhparam.pem 4096
-
-
-
-
-
-
-
