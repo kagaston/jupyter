d663479cb82a9a6c33c0499795f4235f7f4b4b23
Kody Gaston <43728989+kagaston@users.noreply.github.com>
2023-12-16 01:28:16 -0700

    Update build.sh: added error handling removed commented out lines.

--- build.sh
+++ build.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
-# TODO add the CS install section to this build file
-# TODO Add variables variable check --> declare defaults if not found
+# Define default values for variables if not provided
+REPO="${REPO:-kagaston}"
+NAME="${NAME:-jupyter}"
+VERSION="${VERSION:-3.3.1}"
 
-REPO="kagaston"
-NAME="jupyter"
-VERSION="3.3.1"
+# Check if Docker is installed
+if ! command -v docker &> /dev/null; then
+    echo "Docker is not installed. Please install Docker and try again."
+    exit 1
+fi
 
-
+# Build and tag the Docker image
 docker build -t "${REPO}/${NAME}:${VERSION}" -t "${REPO}/${NAME}:latest" .
-docker push --all-tags ${REPO}/${NAME}
 
+# Push all tags to the Docker repository
+docker push --all-tags "${REPO}/${NAME}"
